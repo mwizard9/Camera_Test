5e09814c1f2838b2a18d7c2a0be5f278767ff259
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-13 20:49:01 +0545

    adding items

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg bg-dark">
                 <div className="container-fluid ">
-                    <a className="navbar-brand" style={{ color: 'white' }} href="#"><img src="favicon.png" alt="Camera" width="26" height="26" />Navbar</a>
+                    <a className="navbar-brand" style={{ color: 'white' }} href="#"><img src="favicon.png" alt="Camera" width="26" height="26" />CamRate</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -15,21 +15,19 @@ function Navbar() {
                                 <a className="nav-link active" style={{ color: 'white' }} aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" style={{ color: 'white' }} href="#">Link</a>
+                                <a className="nav-link" style={{ color: 'white' }} href="#">About</a>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" style={{ color: 'white' }} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                    Category
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" style={{ color: 'white' }} href="#">Action</a></li>
-                                    <li><a className="dropdown-item" style={{ color: 'white' }} href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" style={{ color: 'white' }} href="#">Something else here</a></li>
+                                    <li><a className="dropdown-item" href="#">Standard Mode</a></li>
+                                    <li><a className="dropdown-item" href="#">Portrait Mode</a></li>
+                                    <li><a className="dropdown-item" href="#">Low-Light Mode</a></li>
+                                    <li><a className="dropdown-item" href="#">Selfie Mode</a></li>
+  
                                 </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" style={{ color: 'white' }}>Disabled</a>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
