c591a040949b5a1d8718150f58fa2f1efd7d6516
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-16 16:49:02 +0545

    adding about component

---
+++ src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='container my-4'>
+        <h1>Blind Smartphone Manera Test</h1>
+        <h3>In this site you can choose the photo by yourself
+             and choose your own best smartphone camera</h3><br/>
+             <h1>How To Vote</h1>
+             <p>Use your left/right arrow keys to pick the better quality image, or double tap on an image if you are on a phone</p>
+        
+      
+    </div>
+  )
+}
+
+export default About
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,22 +3,22 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-dark">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid ">
-                    <a className="navbar-brand" style={{ color: 'white' }} href="#"><img src="favicon.png" alt="Camera" width="26" height="26" />CamRate</a>
+                    <a className="navbar-brand"  href="#"><img src="favicon.png" alt="Camera" width="26" height="26" />CamRate</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" style={{ color: 'white' }} aria-current="page" href="#">Home</a>
+                                <a className="nav-link active"  aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" style={{ color: 'white' }} href="#">About</a>
+                                <a className="nav-link"  href="#">About</a>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" style={{ color: 'white' }} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link dropdown-toggle"  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Category
                                 </a>
                                 <ul className="dropdown-menu">
@@ -32,7 +32,7 @@ function Navbar() {
                         </ul>
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" style={{ color: 'white' }} type="submit">Search</button>
+                            <button className="btn btn-outline-success"  type="submit">Search</button>
                         </form>
                     </div>
                 </div>
