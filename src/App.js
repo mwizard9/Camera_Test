831c3fbaabf75c74e7b00cd3828d3d324a1fcafe
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-17 18:04:25 +0545

    adding router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import About from './Components/About';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
+import{
+  BrowserRouter as Router,Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
-      <About/>
+      <Router>
+        <Routes>
+          <Route exact path='/about' element={<About/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
