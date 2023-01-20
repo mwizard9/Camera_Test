d3278ae533feb86d78907d638a3ebbef06eb10d0
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-20 19:37:18 +0545

    setting sliding image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,18 +3,27 @@ import React from 'react'
 function Home() {
     return (
 
-        <div id="carouselExampleInterval" class="container carousel slide" style={{backgroundColor:'black',justifyContent:'center'}}data-bs-ride="carousel">
+        <div id="carouselExampleInterval" class="container carousel slide" style={{backgroundColor:'black'}}data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active" style={{}}data-bs-interval="2000">
-            <img src="favicon.png" class="d-block w-20" alt="..."/>
+          <div class="carousel-item active" style={{}}data-bs-interval="3000">
+            <img src="favicon.png"  alt="..."/>
           </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="favicon.png" class="d-block w-20" alt="..."/>
+          <div class="carousel-item" data-bs-interval="3000">
+            <img src="favicon.png"  alt="..."/>
           </div>
-          <div class="container carousel-item">
-            <img src="three.jpeg" style={{height:'520px'}} class="d-block w-20" alt="..."/>
+          <div class="container carousel-item" data-bs-interval="3000">
+            <img src="three.jpeg" style={{height:'512px'}}  alt="..."/>
           </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+
       </div>
     )
 }
