ab8fa417de9047a5f6c9eb94d0dabf582a307a7a
Manish Himalaya <manishhimalaya64@gmail.com>
2023-01-23 21:06:02 +0545

    set a category

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -3,25 +3,31 @@ import React from 'react'
 function Category() {
     return (
         <div>
-            <div class="grid-container">
+            <h1>Choose a Category to Vote</h1>
+            <div class="grid-container" >
 
-                <div class="grid-item"><img src="daytime.jpeg" alt="gunne" style={{ height: '400px', width: '400px' }}/>
-                    Name:m22<br />
-                    <button type="button" >Click for More</button>
+                <div class="grid-item my-1">
+                    <h2 style={{ color: 'white' }}>Standard Mode</h2><br />
+                    <img src="daytime.jpeg" alt="gunne" style={{ height: '350px', width: '350px' }} /><br />
+                    <button className='button my-2' type="button" >VOTE</button>
                 </div>
-                <div class="grid-item"><img src="nighttime.jpeg" alt="gunne" style={{ height: '400px', width: '400px' }} />
-                    Name:m22<br />
-                    <button type="button" >Click for More</button>
+
+                <div class="grid-item my-1">
+                    <h2 style={{ color: 'white' }}>Night Mode</h2><br />
+                    <img src="nighttime.jpeg" alt="gunne" style={{ height: '350px', width: '350px' }} /><br />
+                    <button className='button my-2' type="button" >VOTE</button>
                 </div>
+
+                <div class="grid-item my-1">
+                    <h2 style={{ color: 'white' }}>Portait Mode</h2><br />
+                    <img src="portait.jpeg" alt="gunne" style={{ height: '350px', width: '350px' }} /><br />
+                    <button className='button my-2' type="button" >VOTE</button>
                 </div>
-                <div class="grid-container">
-                <div class="grid-item"><img src="portait.jpeg" alt="gunne" style={{ height: '400px', width: '400px' }} />
-                    Name:m22<br />
-                    <button type="button" >Click for More</button>
-                </div>
-                <div class="grid-item"><img src="image/m22.jpg" alt="gunne" style={{ height: '400px', width: '400px' }} />
-                    Name:m22<br />
-                    <button type="button" >Click for More</button>
+
+                <div class="grid-item my-1">
+                    <h2 style={{ color: 'white' }}>Selfie Mode</h2><br />
+                    <img src="three.jpeg" alt="gunne" style={{ height: '350px', width: '350px' }} /><br />
+                    <button className='button my-2' type="button" >VOTE</button>
                 </div>
             </div>
         </div>
