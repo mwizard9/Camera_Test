18b84ef21e387734b50080e3a291f069a714b994
Manish Himalaya <manishhimalaya64@gmail.com>
2023-02-05 15:34:30 +0545

    adding selectimage images

--- src/Components/SelectImage.js
+++ src/Components/SelectImage.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const SelectImage = () => {
     const imagesArray = [
-        'image/img-1.jpg',
-        'image/img-2.jpg',
-        'image/img-3.jpg',
-        'image/img-4.jpg',
-        'image/pack.jpg',
-        'image/veld.jpg',
-        'image/blank.jpg',
-        'image/daytime.jpeg',
-        'image/nighttime.jpeg',
-        'image/portait.jpeg',
+        'img-1.jpg',
+        'img-2.jpg',
+        'img-3.jpg',
+        'img-4.jpg',
+        'pack.jpg',
+        'veld.jpg',
+        'blank.jpg',
+        'daytime.jpeg',
+        'nighttime.jpeg',
+        'portait.jpeg',
 
     ];
     var usedImages = {};
     var usedImagesCount = 0;
 
-    const displayImage=()=> {
+    const displayImage = () => {
 
         const num = Math.floor(Math.random() * (imagesArray.length));
         const num2 = Math.floor(Math.random() * (imagesArray.length));
@@ -37,36 +37,27 @@ const SelectImage = () => {
             displayImage();
         }
     }
-  return (
-  
-    <>
-    <div>
- 
+    return (
 
+        <>
+            <div className='container'>
 
+                <div className="container mx-3 my-3 d-flex flex-row">
+                    <div className="container">
+                        <img onClick={displayImage} src="blank.jpg" alt='canvas' name="canvas"
+                            style={{ height: '500px', width: '500px' }} /><br />
+                        <span className="thumb">thumb_up</span>
+                    </div>
+                    <div className='container'>
+                        <img onClick={displayImage} src="img-1.jpg" alt='canva' name="canva"
+                            style={{ height: '500px', width: '500px' }} /><br />
+                        <span className="thumb">thumb_up</span>
+                    </div>
+                </div>
 
-    <form name="imageForm">
-
-
-        <div class="container mx-3 my-3 d-flex flex-row">
-            <div class="mx-3 my-3">
-                <img onclick={displayImage} src="image/blank.jpg" alt='canvas' name="canvas"
-                    style={{height: '500px',width: '500px'}}/><br/>
-                <span class="thumb">thumb_up</span>
             </div>
-            <div>
-                <img onclick={displayImage} src="image/img-1.jpg" alt='canva' name="canva"
-                    style={{height: '500px',width: '500px'}}/><br/>
-                <span class="thumb">thumb_up</span>
-            </div>
-        </div>
-
-
-    </form>
-    
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default SelectImage
