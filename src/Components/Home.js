656f3987e0fbb5c905c7bce5cef215d3823885a3
Manish Himalaya <manishhimalaya64@gmail.com>
2023-03-03 18:04:10 +0545

    maintaining some in homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,13 +6,13 @@ function Home() {
 <>    <div id="carouselExampleInterval" className="container carousel slide my-5" style={{ backgroundColor: 'black' }} data-bs-ride="carousel">
       <div className="carousel-inner">
         <div className="carousel-item active" data-bs-interval="3000">
-          <img src="nighttime.jpeg" style={{ height: '400px', width: '400px' }} alt="..." />
+          <img src="nighttime.jpeg" style={{ height: '500px', width: '500px' }} alt="..." />
         </div>
         <div className="carousel-item" data-bs-interval="3000">
-          <img src="daytime.jpeg" style={{ height: '400px', width: '400px' }} alt="..." />
+          <img src="daytime.jpeg" style={{ height: '500px', width: '500px' }} alt="..." />
         </div>
         <div className="container carousel-item" data-bs-interval="3000">
-          <img src="portait.jpeg" style={{ height: '400px', width: '400px' }} alt="..." />
+          <img src="portait.jpeg" style={{ height: '500px', width: '500px' }} alt="..." />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,35 +6,37 @@ function Navbar() {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link  className="navbar-brand"  to="/"><img src="favicon.png" alt="Camera" width="26" height="26" />CamRate</Link  >
+                    <Link className="navbar-brand" to="/"><img src="favicon.png" alt="Camera" width="26" height="26" />CamRate</Link  >
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link  className="nav-link active"  aria-current="page" to="/">Home</Link  >
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link  >
                             </li>
                             <li className="nav-item">
-                                <Link  className="nav-link"  to="/about">About</Link  >
+                                <Link className="nav-link" to="/about">About</Link  >
                             </li>
                             <li className="nav-item dropdown">
-                                <Link  className="nav-link dropdown-toggle"  to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Category
                                 </Link  >
                                 <ul className="dropdown-menu">
-                                    <li><Link  className="dropdown-item" to="/">Standard Mode</Link  ></li>
-                                    <li><Link  className="dropdown-item" to="/">Portrait Mode</Link  ></li>
-                                    <li><Link  className="dropdown-item" to="/">Low-Light Mode</Link  ></li>
-                                    <li><Link  className="dropdown-item" to="/">Selfie Mode</Link  ></li>
-  
+                                    <li><Link className="dropdown-item" to="/">Standard Mode</Link  ></li>
+                                    <li><Link className="dropdown-item" to="/">Portrait Mode</Link  ></li>
+                                    <li><Link className="dropdown-item" to="/">Low-Light Mode</Link  ></li>
+                                    <li><Link className="dropdown-item" to="/">Selfie Mode</Link  ></li>
+
                                 </ul>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success"  type="submit">Search</button>
-                        </form>
+                        <div style={{marginRight:"8%"}}>
+                        <li className="btn btn-outline-success">
+                       
+                            <Link className="nav-link" to="/about">AdminPannel</Link  >
+                        </li>
+                        </div>
                     </div>
                 </div>
             </nav>
