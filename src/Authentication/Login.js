6397e92ed3c4292e00ab03be1a45a0de2e7ba1c2
Manish Himalaya <manishhimalaya64@gmail.com>
2023-06-01 08:08:13 +0545

    updating login and signup

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,56 +1,115 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Login = (props) => {
-    const[credentials, setCredentials] = useState({email:"",password:""})  
-    let history = useNavigate()
-    const handleSubmit= async(e)=>{
-        e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+  let history = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-         },
-      body: JSON.stringify({email:credentials.email,password:credentials.password})
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password }),
     });
-    const json=await response.json()
-    console.log(json)
-    if (json.success){
-        //save the auth token and redirect
-        localStorage.setItem('token',json.jwtData);
-        history("/selectImage")
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      //save the auth token and redirect
+      localStorage.setItem("usertoken", JSON.stringify(json))
+      // localStorage.setItem('token', json.jwtData);
+      history('/selectImage');
+    } else {
+      alert('Invalid credentials');
     }
-    else{
-        alert("invalid credentials")
-    }
-        
-    }
+  };
 
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-    }
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '50px',
+    background: 'linear-gradient(120deg, #e0eafc, #cfdef3)',
+    padding: '20px',
+    borderRadius: '5px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    maxWidth: '400px',
+    margin: '0 auto',
+  };
+  const headingStyle = {
+    fontSize: '24px',
+    marginBottom: '20px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  };
+
+  const formGroupStyle = {
+    marginBottom: '20px',
+    width: '100%',
+  };
+
+  const buttonStyle = {
+    width: '100%',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    padding: '10px',
+    cursor: 'pointer',
+  };
+
+  const signupLinkStyle = {
+    textAlign: 'center',
+    marginTop: '10px',
+  };
+
   return (
-    <div className='container' >
-      <h1>Please,first login to vote</h1>
-     <form onSubmit={handleSubmit}>
-  <div className="mb-3">
-    <label htmlFor="emai1" className="form-label">Email address</label>
-    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp"/>
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>Please, first login to vote</h1>
+      <form onSubmit={handleSubmit}>
+        <div style={formGroupStyle}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            value={credentials.email}
+            onChange={onChange}
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div style={formGroupStyle}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={credentials.password}
+            onChange={onChange}
+            id="password"
+            name="password"
+          />
+        </div>
 
-  </div>
-  <div className="mb-3">
-    <label htmlFor="password1" className="form-label">Password</label>
-    <input type="password" className="form-control" value={credentials.password} onChange={onChange} id="password" name="password"/>
-  </div>
-  
-  <button type="submit" className="btn btn-primary" >Submit</button><br/>
-  <p>If you don't have account then Register here</p>
-  <Link className="btn btn-outline-primary mx-1" to="/signup" role="button">Signup</Link>
-</form>
+        <button type="submit" className="btn btn-primary" style={buttonStyle}>
+          Submit
+        </button>
+      </form>
+      <div style={signupLinkStyle}>
+        <p>If you don't have an account, then&nbsp;</p>
+        <Link to="/signup">Sign up here</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Authentication/Signup.js
+++ src/Authentication/Signup.js
@@ -1,61 +1,103 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    const[credentials, setCredentials] = useState({name:"",email:"",password:"",cpassword:""})  
-    let history = useNavigate()
-    const handleSubmit= async(e)=>{
-        e.preventDefault();
-        const{name,email,password} = credentials;
-        const response = await fetch("http://localhost:5000/api/auth/createUser", {
+  const [credentials, setCredentials] = useState({ name: '', email: '', password: '', cpassword: '' });
+  let history = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, email, password } = credentials;
+    const response = await fetch('http://localhost:5000/api/auth/createUser', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-         },
-      body: JSON.stringify({name,email,password})
+      },
+      body: JSON.stringify({ name, email, password }),
     });
-    const json=await response.json()
-    console.log(json)
-    if (json.success){
-        //save the auth token and redirect
-        localStorage.setItem('token',json.jwtData);
-        history("/")
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem('token', json.jwtData);
+      history('/login');
+    } else {
+      alert('Invalid credentials');
     }
-    else{
-        alert("invalid credentials")
-    }
-        
-    }
+  };
 
-    const onChange = (e)=>{
-        setCredentials({...credentials, [e.target.name]: e.target.value})
-    }
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '50px',
+  };
+
+  const formStyle = {
+    width: '300px',
+    padding: '20px',
+    background: '#f8f9fa',
+    borderRadius: '5px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  };
+
+  const inputStyle = {
+    marginBottom: '10px',
+    padding: '8px',
+    borderRadius: '3px',
+    border: '1px solid #ccc',
+    width: '100%',
+  };
+
+  const buttonStyle = {
+    marginTop: '10px',
+    padding: '8px 12px',
+    borderRadius: '3px',
+    border: 'none',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    cursor: 'pointer',
+    width: '100%',
+  };
+
   return (
-    <div className='container ' >
-      <form onSubmit={handleSubmit}>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-    <input type="text" className="form-control" name="name" id="name" onChange={onChange} aria-describedby="emailHelp"/>
-
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email" name="email" onChange={onChange}  aria-describedby="emailHelp"/>
-
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" onChange={onChange} name="password"  id="password"minLength={5} required/>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Conform Password</label>
-    <input type="password" className="form-control" onChange={onChange} name="password"  id="cpassword" minLength={5} required/>
-  </div>
- 
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+    <div style={containerStyle}>
+      <form style={formStyle} onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" name="name" id="name" onChange={onChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" id="email" name="email" onChange={onChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" onChange={onChange} name="password" id="password" minLength={5} required />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={onChange}
+            name="cpassword"
+            id="cpassword"
+            minLength={5}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" style={buttonStyle}>
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
