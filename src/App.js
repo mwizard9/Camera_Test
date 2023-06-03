a03b91be3ffa8ff4b12d96d4675e315cfba076c8
Manish Himalaya <manishhimalaya64@gmail.com>
2023-06-03 07:22:14 +0545

    adding private routing for select image

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import './App.css';
-import About from './Components/About';
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-import {
-  BrowserRouter as Router, Routes, Route
-} from "react-router-dom";
-import Category from './Components/Category';
-import SelectImage from './Components/SelectImage';
+import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Category from "./Components/Category";
+import SelectImage from "./Components/SelectImage";
 import Login from "./Authentication/Login";
-import Signup from "./Authentication/Signup"
-import ALogin from './AdminSite/Login';
-import Result from './Components/Result';
-import UsedDevice from './Components/UsedDevice';
-import AdminDashboard from './AdminSite/AdminDashboard';
-import PhoneResult from './Result/Result';
-import UserTable from './AdminSite/UserData';
-import ImageList from './AdminSite/ImageData';
-import AdminRegistration from './AdminSite/adminRegistration';
-import ImageUploadForm from './AdminSite/UploadImage';
+import Signup from "./Authentication/Signup";
+import ALogin from "./AdminSite/Login";
+import Result from "./Components/Result";
+import UsedDevice from "./Components/UsedDevice";
+import AdminDashboard from "./AdminSite/AdminDashboard";
+import PhoneResult from "./Result/Result";
+import UserTable from "./AdminSite/UserData";
+import ImageList from "./AdminSite/ImageData";
+import AdminRegistration from "./AdminSite/adminRegistration";
+import ImageUploadForm from "./AdminSite/UploadImage";
+import PrivateOutlet from "./PrivateRoute/Private";
 
 function App() {
   return (
     <div className="App">
-       <Router basename="/bliendCameraTest/">
+      <Router basename="/bliendCameraTest/">
         <Navbar />
         <Routes>
-        {/* <Route exact path='/' element={< PhoneResult/>}></Route> */}
-          <Route exact path='/' element={<Home />}></Route>
-          <Route exact path='/about' element={<About />}></Route>
-          <Route exact path='/category' element={<Category />}></Route>
-          <Route exact path='/selectImage' element={<SelectImage />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/signup" element={<Signup />}>
+          {/* <Route exact path='/' element={< PhoneResult/>}></Route> */}
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/category" element={<Category />}></Route>
+          <Route path="/selectImage" element={<PrivateOutlet />}>
+            <Route index element={<SelectImage />} />
           </Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
           <Route exact path="/alogin" element={<ALogin />}></Route>
-        <Route exact path="/result" element={<Result/>}></Route>
-        <Route exact path="/useddevice" element={<UsedDevice/>}></Route>
-        <Route exact path="/admind" element={<AdminDashboard/>}></Route>
-        <Route exact path="/userdetails" element={<UserTable/>}></Route>
-        <Route exact path="/imagedetails" element={<ImageList/>}></Route>
-        <Route exact path="/createadmin" element={<AdminRegistration/>}></Route>
-        <Route exact path="/uploadimage" element={<ImageUploadForm/>}></Route>
+          <Route exact path="/result" element={<Result />}></Route>
+          <Route exact path="/useddevice" element={<UsedDevice />}></Route>
+          <Route exact path="/admind" element={<AdminDashboard />}></Route>
+          <Route exact path="/userdetails" element={<UserTable />}></Route>
+          <Route exact path="/imagedetails" element={<ImageList />}></Route>
+          <Route
+            exact
+            path="/createadmin"
+            element={<AdminRegistration />}
+          ></Route>
+          <Route
+            exact
+            path="/uploadimage"
+            element={<ImageUploadForm />}
+          ></Route>
         </Routes>
       </Router>
     </div>
