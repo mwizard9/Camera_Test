d5435360f011d6675be880a81d4fdaeb29513111
Manish Himalaya <manishhimalaya64@gmail.com>
2023-06-03 07:21:58 +0545

    adding private outlet component

--- src/PrivateRoute/Private.js
+++ src/PrivateRoute/Private.js
@@ -31,24 +31,27 @@ export default function PrivateOutlet() {
 //   };
 
   useEffect(() => {
-    const accessToken = JSON.parse(localStorage.getItem("usertoken"));
-    console.log(accessToken?.expiryDate,'this is access tokens expirydate form services')
-    
-    const refreshToken = JSON.parse(localStorage.getItem("refreshToken"));
-    console.log(refreshToken,'this is refresh token')
-    const currentDateTime = Math.floor(new Date().getTime() / 1000); // Calculate current date in seconds
-    const expiryDateTime = Math.floor(
-      new Date(accessToken?.expiryDate).getTime() / 1000
-    );
-    
-      console.log("hellooooooooooo")
-      if (currentDateTime >= expiryDateTime) {
-        localStorage.clear();
-        // Perform any additional actions when the token expires
-        // For example, show a notification or redirect to the login page
-        notification.warning("Your session has expired. Please login again.");
-        navigate("/");
-      }
+    const checkExpiryDate = () => {
+        const currentDateTime = Math.floor(new Date().getTime() / 1000); // Calculate current date in seconds
+        const expiryDateTime = Math.floor(
+          new Date(accessToken?.expiryDate).getTime() / 1000
+        );
+        console.log(currentDateTime,'this is current date')
+        console.log(expiryDateTime,'this is ex date')
+  
+        if (currentDateTime >= expiryDateTime) {
+          localStorage.clear();
+         
+          notification.warning("Your session has expired. Please login again.");
+          navigate("/");
+        }
+      };
+  
+      const interval = setInterval(checkExpiryDate, 5000); // Check every 5 seconds
+  
+      return () => {
+        clearInterval(interval); // Clean up the interval when the component unmounts
+      };
   }, []);
 
   console.log(accessToken, "this is access");
